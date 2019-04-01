c5c83478e4286f16ea7326ef7a26dc7b3c5f7a68
raghavendranekkanti <raghavendra.nekkanti@inmar.com>
2019-04-01 15:48:22 +0530

    editor integration in progress

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Editor from './Editor';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        code: '// type your code... \n',
+    }
+  }
+
+  onChange = (newValue, e) => {
+    console.log('onChange', newValue, e);
+  }
+
+  editorDidMount = (editor) => {
+    console.log('editorDidMount', editor, editor.getValue(), editor.getModel());
+    this.editor = editor;
+  }
+
+  changeEditorValue = (value) => {
+    if (this.editor) {
+      this.editor.setValue(value);
+    }
+  }
   render() {
+    const { code } = this.state;
+    const options = {
+      selectOnLineNumbers: true,
+      roundedSelection: false,
+      readOnly: false,
+      cursorStyle: 'line',
+      automaticLayout: false,
+      lineNumbers: 'on'
+    };
     return (
+      <React.Fragment>
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Monaco Editor
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
       </div>
+      <Editor
+        height="500"
+        language="javascript"
+        value={code}
+        options={options}
+        onChange={this.onChange}
+        editorDidMount={this.editorDidMount}
+      />
+      </React.Fragment>
     );
   }
 }
